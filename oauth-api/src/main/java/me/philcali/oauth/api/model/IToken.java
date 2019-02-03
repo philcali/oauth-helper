4e52543cde428085fd04c9d73e32634131152fc4
Philip Cali <philip.cali@gmail.com>
2019-02-03 19:49:51 +0000

    Providing another impl for later.

--- oauth-api/src/main/java/me/philcali/oauth/api/model/IToken.java
+++ oauth-api/src/main/java/me/philcali/oauth/api/model/IToken.java
@@ -2,6 +2,9 @@ package me.philcali.oauth.api.model;
 
 import java.util.Map;
 
+import me.philcali.zero.lombok.annotation.Data;
+
+@Data
 public interface IToken {
     String getAccessToken();
     String getApi();
